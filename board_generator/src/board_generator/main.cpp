f352d84387ecdfe04d70d66860573e263f5aaf41
Cedric Priscal <cedric.priscal@gmail.com>
2017-12-29 09:44:48 -0800

    Adding engine board

--- board_generator/src/board_generator/main.cpp
+++ board_generator/src/board_generator/main.cpp
@@ -68,6 +68,22 @@ int main()
         board.save();
     }
 
+    {
+        Board board("engine");
+        board.add_module(&edge);
+        board_add_module(&input);
+        board.add_module(&pic16f1829lin);
+        board.add_module(&switch_low_side);  // HEATER_AIR_BLOWER
+        board.add_module(&switch_low_side);  // COLD_STARTING_VALVE
+        board.add_module(&switch_high_side); // IGNITION
+        board.add_module(&switch_high_side); // ECU
+        board.add_module(&switch_high_side); // FUEL_PUMP
+        board.add_module(&analog_input);     // CYLINDER_HEAD_TEMPERATURE
+        board.add_module(&analog_input);     // FUEL_GAUGE (resistance measurement)
+        board_add_module(&input);
+        board.add_module(&edge);
+    }
+
     return 0;
 }
 
