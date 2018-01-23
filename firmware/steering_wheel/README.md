86e7bad339d74b5e6ebfc9474c58dec89d89a58f
Cedric Priscal <cedric.priscal@gmail.com>
2018-01-22 20:08:41 -0800

    steering_wheel firmware implementation

---
+++ firmware/steering_wheel/README.md
@@ -0,0 +1,13 @@
+
+# Turn signals
+
+Same as origin.
+
+# Headline dimmer
+
+When starting the car, the lighting is set to low beam.
+
+Pulling the handle will cause a headlight flash (both low and high beam on). If releasing before 1 second, the lighting goes back to its previous setting. If releasing after 1 second, the lighting will toggle between low and high beam.
+
+`LIGHTS` bit 0: low beam bulbs
+`LIGHTS` bit 1: high beam bulbs
--- firmware/steering_wheel/main.c
+++ firmware/steering_wheel/main.c
@@ -59,8 +59,84 @@ void main(void)
     // Enable the Peripheral Interrupts
     INTERRUPT_PeripheralInterruptEnable();
 
+    int dimmer_state = 0;
+    uint16_t dimmer_start;
+
+    // Start with low-beam ON
+    LIGHTS_Data[0] = 1;
+    uint8_t previous_light = 1;
+
     while (1)
     {
+        // D0: IGNITION_STARTER_POS_1
+        // D1: IGNITION_STARTER_POS_2
+        if (!D1_GetValue()) {
+            IGNITION_STARTER_Data[0] = 2;
+        } else if (!D0_GetValue()) {
+            IGNITION_STARTER_Data[0] = 1;
+        } else {
+            IGNITION_STARTER_Data[0] = 0;
+        }
+
+        // D2: WINDSHIELD_WIPERS_SLOW
+        // D3: WINDSHIELD_WIPERS_FAST
+        if (!D3_GetValue()) {
+            WIPERS_Data[0] = 255;
+        } else if (!D2_GetValue()) {
+            WIPERS_Data[0] = 128;
+        } else {
+            WIPERS_Data[0] = 0;
+        }
+
+        // D4: TURN_SIGNAL_LEFT
+        if (!D4_GetValue()) {
+            TURN_SIGNAL_Data[0] |= 1;
+        } else {
+            TURN_SIGNAL_Data[0] &= ~1;
+        }
+
+        // D5: TURN_SIGNAL_RIGHT
+        if (!D5_GetValue()) {
+            TURN_SIGNAL_Data[0] |= 2;
+        } else {
+            TURN_SIGNAL_Data[0] &= ~2;
+        }
+
+        // D6: HEADLIGHT_DIMMER
+        if (dimmer_state == 0) {
+            // lever is released, wait for it to be activated
+            if (!D6_GetValue()) {
+                // Turn on both low and high beam
+                previous_light = LIGHTS_Data[0];
+                LIGHTS_Data[0] = 3;
+                dimmer_start = TMR1_ReadTimer();
+                dimmer_state = 1;
+            }
+        } else if (dimmer_state == 1) {
+            // lever was pressed, wait for either 1 second or a release
+            if (D6_GetValue()) {
+                // lever released before 1 second, restore previous light state
+                LIGHTS_Data[0] = previous_light;
+                dimmer_state = 0;
+            } else {
+                uint16_t dimmer_duration = TMR1_ReadTimer() - dimmer_start;
+                if (dimmer_duration > 1000) {
+                    // 1 second has elapsed
+                    dimmer_state = 2;
+                }
+            }
+        } else if (dimmer_state == 2) {
+            // lever has been pressed for more than 1 second, wait for a release
+            if (D6_GetValue()) {
+                previous_light = previous_light ^ 3;
+                LIGHTS_Data[0] = previous_light;
+                dimmer_state = 0;
+            }
+        }
+
+        // D7: HORN_BUTTON
+        HORN_Data[0] = !D7_GetValue();
+        
         LIN_handler();
     }
 }
